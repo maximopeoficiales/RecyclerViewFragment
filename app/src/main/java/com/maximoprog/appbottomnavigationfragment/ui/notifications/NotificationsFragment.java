f162521add173a6d63099e930e943665d5bd638e
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-05-20 12:05:44 -0500

    se agrego binding al fragment

--- app/src/main/java/com/maximoprog/appbottomnavigationfragment/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/maximoprog/appbottomnavigationfragment/ui/notifications/NotificationsFragment.java
@@ -4,17 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.maximoprog.appbottomnavigationfragment.R;
+import com.maximoprog.appbottomnavigationfragment.databinding.FragmentNotificationsBinding;
 
 import es.dmoral.toasty.Toasty;
 
@@ -26,24 +22,21 @@ public class NotificationsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        FragmentNotificationsBinding root = FragmentNotificationsBinding.inflate(inflater, container, false);
 //        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-            }
-        });
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+////                textView.setText(s);
+//            }
+//        });
 
-        Button btnGuardarMenu = root.findViewById(R.id.btnGuardarMenu);
-        EditText txtPrecio = root.findViewById(R.id.txtPrecioMenu);
-        EditText txtMenu = root.findViewById(R.id.txtNameMenu);
 
-        btnGuardarMenu.setOnClickListener(new View.OnClickListener() {
+        root.btnGuardarMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String precio = txtPrecio.getText().toString();
-                String menu = txtMenu.getText().toString();
+                String precio = root.txtPrecioMenu.getText().toString();
+                String menu = root.txtNameMenu.getText().toString();
                 if (!precio.equals("") && !menu.equals("")) {
                     if (Double.parseDouble(precio) > 0) {
 //                        txtResult.setText("El precio a pagar por " + nMenus + " menus es: S./ " + Double.parseDouble(nMenus) * Double.parseDouble(precio));
@@ -56,6 +49,6 @@ public class NotificationsFragment extends Fragment {
                 }
             }
         });
-        return root;
+        return root.getRoot();
     }
 }
